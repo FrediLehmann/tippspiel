e50b057364882c037d2fe4ac427bfd9ed1dd7764
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-05-19 21:43:57 +0200

    prettier

--- src/app/[locale]/(authenticated)/layout.tsx
+++ src/app/[locale]/(authenticated)/layout.tsx
@@ -3,7 +3,11 @@ import { redirect } from "@/lib/translation";
 
 export const dynamic = "force-dynamic";
 
-export default async function SignedInlayout({ children }: { children: React.ReactNode }) {
+export default async function SignedInlayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const supabase = createServerClient();
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
