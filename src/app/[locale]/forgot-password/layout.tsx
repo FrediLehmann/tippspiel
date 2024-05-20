b76e942c7dba85e7849589be919099ff56177462
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-05-20 16:29:31 +0200

    create forgot password page

---
+++ src/app/[locale]/forgot-password/layout.tsx
@@ -0,0 +1,5 @@
+import { ProvideMessages } from "@/components";
+
+export default function ForgotPasswordLayout({ children }: { children: React.ReactNode }) {
+  return <ProvideMessages namespaces={["common", "ForgotPassword"]}>{children}</ProvideMessages>;
+}
