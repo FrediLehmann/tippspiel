8d630065bde86747696f0c3dca97a6fc11e61284
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-05-29 20:36:10 +0200

    Add DataTable component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as DataTable } from "./DataTable";
 export { default as LanguageSwitch } from "./LanguageSwitch";
 export { default as PasswordInput } from "./PasswordInput";
 export { default as ProvideMessages } from "./ProvideMessages";
