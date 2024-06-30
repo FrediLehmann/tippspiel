bd5ae3f73c935e9ebf771305787bdfee4832d9f0
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-06-30 19:49:56 +0200

    Use games

--- src/app/[locale]/(authenticated)/[game]/cockpit/components/PredictionGameSelect/PredictionGameSelect.tsx
+++ src/app/[locale]/(authenticated)/[game]/cockpit/components/PredictionGameSelect/PredictionGameSelect.tsx
@@ -1,16 +1,31 @@
 'use client';
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
+import {
+	selectedPredictionGame,
+	setSelectedGame,
+	useAppDispatch,
+	useAppSelector
+} from '@/lib/store';
 
 export default function PredictionGameStats() {
+	const dispatch = useAppDispatch();
+	const selectedGame = useAppSelector(selectedPredictionGame);
+	const games = useAppSelector((state) => state.games.games);
+
 	return (
-		<Select value="erste-manschaft-fc-schmitten">
+		<Select
+			value={selectedGame?.id.toString() ?? '0'}
+			onValueChange={(value) => dispatch(setSelectedGame(Number(value)))}>
 			<SelectTrigger className="w-full md:w-96">
 				<SelectValue />
 			</SelectTrigger>
 			<SelectContent>
-				<SelectItem value="erste-manschaft-fc-schmitten">Erste Manschaft FC Schmitten</SelectItem>
-				<SelectItem value="zweite-manschaft-fc-schmitten">Zweite Manschaft FC Schmitten</SelectItem>
+				{games.map((game) => (
+					<SelectItem key={game.id} value={game.id.toString()}>
+						{game.name} ({game.team})
+					</SelectItem>
+				))}
 			</SelectContent>
 		</Select>
 	);
--- src/lib/store/predictionGames/selectedPredictionGame.ts
+++ src/lib/store/predictionGames/selectedPredictionGame.ts
@@ -1,7 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 const selectedPredictionGame = createSelector(
-	[(state) => state.games, (state) => state.selectedGame],
+	[(state) => state.games.games, (state) => state.games.selectedGame],
 	(games, selectedGame) => games.find((game: { id: string }) => game.id === selectedGame)
 );
 
