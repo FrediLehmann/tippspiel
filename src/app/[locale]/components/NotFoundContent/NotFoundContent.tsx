55f2b68bfbd306d2f19cdbafa2e22977b698ed83
Frederic Lehmann <frederic.lehmann@bluewin.ch>
2024-06-30 16:57:29 +0200

    remove duplicate class

--- src/app/[locale]/components/NotFoundContent/NotFoundContent.tsx
+++ src/app/[locale]/components/NotFoundContent/NotFoundContent.tsx
@@ -7,7 +7,7 @@ import { Separator } from '@/components/ui';
 export default function NotFoundContent() {
 	const t = useTranslations('404');
 	return (
-		<div className="flex gap-6 mt-20 mx-auto items-center mx-auto">
+		<div className="flex gap-6 mt-20 mx-auto items-center">
 			<div className="font-semibold text-lg">404</div>
 			<Separator orientation="vertical" className="h-20" />
 			<div>
